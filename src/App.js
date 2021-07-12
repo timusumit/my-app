faf3daf2b08b064fddb5ecf2e338f8cbb2fc9004
Sumit Maharjan <timusumit@gmail.com>
2021-07-12 22:02:58 +0545

    before props.children

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavouritesPage from './pages/Favourites';
 import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import MainNavigation from './components/layout/MainNavigation';
+
 function App() {
 
   return (
     <div>
+     <MainNavigation/>
       <Switch>
       <Route path='/' exact={true}>
         <AllMeetupsPage></AllMeetupsPage>
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,6 +1,35 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_DATA =[
+{
+   id:'m1',
+   title:'This is first meetup',
+   image:'https://thumbs.dreamstime.com/z/nice-fifth-most-populous-city-france-capital-alpes-maritimes-d%C3%A9partement-panorama-old-town-126515115.jpg',
+   address:'',
+   description:'This is first meetup, amazing which you definately should not miss.' 
+},
+{
+    id:'m2',
+    title:'This is second meetup',
+    image:'https://thumbs.dreamstime.com/z/nice-fifth-most-populous-city-france-capital-alpes-maritimes-d%C3%A9partement-panorama-old-town-126515115.jpg',
+    address:'',
+    description:'This is second meetup, amazing which you definately should not miss.' 
+ },
+ {
+    id:'m3',
+    title:'This is third meetup',
+    image:'https://thumbs.dreamstime.com/z/nice-fifth-most-populous-city-france-capital-alpes-maritimes-d%C3%A9partement-panorama-old-town-126515115.jpg',
+    address:'',
+    description:'This is third meetup, amazing which you definately should not miss.' 
+ }
+];
+
 function AllMeetupsPage(){
     return(
-        <div>All Meetups Page</div>
+        <section>
+            <h1>All Meetups</h1>
+           <MeetupList meetups={DUMMY_DATA}/>
+        </section>
     );
 }
 export default AllMeetupsPage;
