744a72a11d6a50fd2b67904eafa76be0eb19cc2d
Sumit Maharjan <timusumit@gmail.com>
2021-07-17 18:02:02 +0545

    mettupform

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,27 @@
+import {useHistory} from 'react-router-dom';
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage(){
+  const history =  useHistory();
+    function AddMeetupHandler(meetupData){
+      fetch(
+          'https://react-getting-started-a7a37-default-rtdb.firebaseio.com/meetups.json',
+          {
+              method:'POST',
+              body:JSON.stringify(meetupData),
+              headers:{
+                  'Content-Type':'application/json'
+              }
+          }
+          ).then(() => {
+              history.replace('/');
+          });  
+    }
     return(
-        <div>New Meetup Page</div>
+       <section>
+           <h1>Add New Meetup</h1>
+           <NewMeetupForm onAddMeetup={AddMeetupHandler}/>
+       </section>
     );
 }
 export default NewMeetupPage;
